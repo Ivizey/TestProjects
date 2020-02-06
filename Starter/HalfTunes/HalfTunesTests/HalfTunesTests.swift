85aa15762f333ced3db6dec7a94f79c0472177d6
Pavel Bondar <ivizey@gmail.com>
2020-02-06 12:38:07 +0200

    testValidCallToiTunesGetsHTTPStatusCode200

--- Starter/HalfTunes/HalfTunesTests/HalfTunesTests.swift
+++ Starter/HalfTunes/HalfTunesTests/HalfTunesTests.swift
@@ -42,4 +42,32 @@ class HalfTunesTests: XCTestCase {
     sut = nil
     super.tearDown()
   }
+  
+  // Asynchronous test: success fast, failure slow
+  func testValidCallToiTunesGetsHTTPStatusCode200() {
+    // given
+    let url =
+      URL(string: "https://itunes.apple.com/search?media=music&entity=song&term=abba")
+    // 1
+    let promise = expectation(description: "Status code: 200")
+
+    // when
+    let dataTask = sut.dataTask(with: url!) { data, response, error in
+      // then
+      if let error = error {
+        XCTFail("Error: \(error.localizedDescription)")
+        return
+      } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+        if statusCode == 200 {
+          // 2
+          promise.fulfill()
+        } else {
+          XCTFail("Status code: \(statusCode)")
+        }
+      }
+    }
+    dataTask.resume()
+    // 3
+    wait(for: [promise], timeout: 5)
+  }
 }
