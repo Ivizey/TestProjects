00a1ea691240935d503ebe60542707fdc9ab9ef8
Pavel Bondar <ivizey@gmail.com>
2020-02-06 12:45:16 +0200

    testCallToiTunesCompletes

--- Starter/HalfTunes/HalfTunesTests/HalfTunesTests.swift
+++ Starter/HalfTunes/HalfTunesTests/HalfTunesTests.swift
@@ -70,4 +70,26 @@ class HalfTunesTests: XCTestCase {
     // 3
     wait(for: [promise], timeout: 5)
   }
+  
+  func testCallToiTunesCompletes() {
+    // given
+    let url =
+      URL(string: "https://itune.apple.com/search?media=music&entity=song&term=abba")
+    let promise = expectation(description: "Completion handler invoked")
+    var statusCode: Int?
+    var responseError: Error?
+
+    // when
+    let dataTask = sut.dataTask(with: url!) { data, response, error in
+      statusCode = (response as? HTTPURLResponse)?.statusCode
+      responseError = error
+      promise.fulfill()
+    }
+    dataTask.resume()
+    wait(for: [promise], timeout: 5)
+
+    // then
+    XCTAssertNil(responseError)
+    XCTAssertEqual(statusCode, 200)
+  }
 }
