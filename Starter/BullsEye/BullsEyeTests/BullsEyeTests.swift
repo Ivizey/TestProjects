ca47ec52e7e4867bff9a168a0e2e737ff8f79daf
Pavel Bondar <ivizey@gmail.com>
2020-02-06 11:41:48 +0200

    add testScoreIsComputed

--- Starter/BullsEye/BullsEyeTests/BullsEyeTests.swift
+++ Starter/BullsEye/BullsEyeTests/BullsEyeTests.swift
@@ -29,13 +29,30 @@
 import XCTest
 @testable import BullsEye
 
+var sut: BullsEyeGame!
+
 class BullsEyeTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+  
+  override func setUp() {
+    super.setUp()
+    sut = BullsEyeGame ()
+    sut.startNewGame ()
+  }
+  
+  override func tearDown() {
+    sut = nil
+    super.tearDown()
+  }
+  
+  func testScoreIsComputed() {
+    // 1. given
+    let guess = sut.targetValue + 5
+    
+    // 2. when
+    sut.check(guess: guess)
+    
+    // 3. then
+    XCTAssertEqual(sut.scoreRound, 95, "Score computed from guess is wrong")
+  }
+  
 }
