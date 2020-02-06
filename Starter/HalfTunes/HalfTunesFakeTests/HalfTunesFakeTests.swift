ba96fbbe20d9c70f17bcf3d9dbe713c022940947
Pavel Bondar <ivizey@gmail.com>
2020-02-06 13:00:29 +0200

    setup test case

--- Starter/HalfTunes/HalfTunesFakeTests/HalfTunesFakeTests.swift
+++ Starter/HalfTunes/HalfTunesFakeTests/HalfTunesFakeTests.swift
@@ -27,27 +27,20 @@
 /// THE SOFTWARE.
 
 import XCTest
+@testable import HalfTunes
 
 class HalfTunesFakeTests: XCTestCase {
 
+    var sut: SearchViewController!
+
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+      super.setUp()
+      sut = UIStoryboard(name: "Main", bundle: nil)
+        .instantiateInitialViewController() as? SearchViewController
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+      sut = nil
+      super.tearDown()
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
